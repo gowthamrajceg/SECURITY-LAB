26c8e3d3c37588d24c6cae67a116c3a3b92d63c5
GOWTHAMRAJ <gowthamrajp06@gmail.com>
2019-09-09 13:47:35 +0530

    Add files via upload

---
+++ attacks/index.php
@@ -0,0 +1,12 @@
+<form action='sql1.php' method="post">
+email:
+
+<input type="email" name="email" required="required"/><br>
+password:
+<input type="password" name="password"/><br>
+remember:
+<input type="checkbox" name="remember_me" value="Remember me"/><br>
+
+<input type="submit" value="Submit"/>
+
+</form>
